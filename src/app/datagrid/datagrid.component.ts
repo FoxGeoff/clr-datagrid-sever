b6dfe2ff424f1c451dff66a77b317a4f9cc0b09c
Geoff Fox <fox.geoffrey@gmail.com>
2020-12-11 15:21:42 -0800

    add Datagrid Component

--- src/app/datagrid/datagrid.component.ts
+++ src/app/datagrid/datagrid.component.ts
@@ -2,14 +2,52 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-datagrid',
+  providers: [Inventory],
   templateUrl: './datagrid.component.html',
   styleUrls: ['./datagrid.component.css']
 })
 export class DatagridComponent implements OnInit {
 
-  constructor() { }
+  users: User[];
+  total: number;
+  loading: boolean = true;
+  page: number;
 
-  ngOnInit(): void {
+  constructor(private inventory: Inventory) {
+    inventory.size = 103;
+    this.inventory.latency = 500;
+    inventory.reset();
   }
 
+  ngOnInit(): void {
+
+  }
+
+  refresh(state: ClrDatagridStateInterface) {
+    this.loading = true;
+    const filters: { [prop: string]: any[] } = {};
+    if (state.filters) {
+      for (const filter of state.filters) {
+        const { property, value } = <{ property: string; value: string }>filter;
+        filters[property] = [value];
+      }
+    }
+    if (!state.page) {
+      state.page = {
+        from: 0,
+        to: 9,
+        size: 10,
+      };
+    }
+    this.inventory
+      .filter(filters)
+      .sort(<{ by: string; reverse: boolean }>state.sort)
+      .fetch(state.page.from, state.page.size)
+      .then((result: FetchResult) => {
+        this.users = result.users;
+        this.total = result.length;
+        this.loading = false;
+      });
+  }
 }
+
