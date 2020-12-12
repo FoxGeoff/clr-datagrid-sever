9c254d68e1a5b8f48dc15ccfe42d5f5a50d970e4
Geoff Fox <fox.geoffrey@gmail.com>
2020-12-11 16:32:58 -0800

    Fix color filter

--- src/app/utils/color-filters.ts
+++ src/app/utils/color-filters.ts
@@ -1,50 +1,50 @@
-import { Component, EventEmitter } from "@angular/core";
-import { ClrDatagridStringFilterInterface } from "@clr/angular";
-import { User } from "../inventory/user";
-import { COLORS } from "../inventory/values";
+import {Component, EventEmitter} from "@angular/core";
+import {ClrDatagridStringFilterInterface} from "@clr/angular";
+import {User} from "../inventory/user";
+import {COLORS} from "../inventory/values";
 
 
 @Component({
-  selector: "app-color-filter",
-  template: `
+    selector: "app-color-filter",
+    template: `
         <span *ngFor="let color of allColors" class="color-square color-selectable"
             (click)="toggleColor(color)"
             [style.backgroundColor]="color"
             [class.color-selected]="selectedColors[color]"></span>`,
-  styleUrls: ["../app.component.css"]
+    styleUrls: ["../datagrid/datagrid.component.css"]
 })
 export class ColorFilterComponent implements ClrDatagridStringFilterInterface<User> {
-  allColors = COLORS;
-  selectedColors: { [color: string]: boolean } = {};
-  nbColors = 0;
+    allColors = COLORS;
+    selectedColors: {[color: string]: boolean} = {};
+    nbColors = 0;
 
-  changes: EventEmitter<any> = new EventEmitter<any>(false);
+    changes: EventEmitter<any> = new EventEmitter<any>(false);
 
-  constructor() { }
+    constructor(){}
 
-  onI
+    onI
 
-  listSelected(): string[] {
-    const list: string[] = [];
-    for (const color in this.selectedColors) {
-      if (this.selectedColors[color]) {
-        list.push(color);
-      }
+    listSelected(): string[] {
+        const list: string[] = [];
+        for (const color in this.selectedColors) {
+            if (this.selectedColors[color]) {
+                list.push(color);
+            }
+        }
+        return list;
     }
-    return list;
-  }
 
-  toggleColor(color: string) {
-    this.selectedColors[color] = !this.selectedColors[color];
-    this.selectedColors[color] ? this.nbColors++ : this.nbColors--;
-    this.changes.emit(true);
-  }
+    toggleColor(color: string) {
+        this.selectedColors[color] = !this.selectedColors[color];
+        this.selectedColors[color] ? this.nbColors++ : this.nbColors--;
+        this.changes.emit(true);
+    }
 
-  accepts(user: User) {
-    return this.nbColors === 0 || this.selectedColors[user.color];
-  }
+    accepts(user: User) {
+        return this.nbColors === 0 || this.selectedColors[user.color];
+    }
 
-  isActive(): boolean {
-    return this.nbColors > 0;
-  }
+    isActive(): boolean {
+        return this.nbColors > 0;
+    }
 }
